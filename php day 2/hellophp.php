6ae49316619a01c2292e499702908bad288615e2
khandhardhaval100 <84770522+khandhardhaval100@users.noreply.github.com>
2021-05-28 09:55:08 +0530

    Add files via upload

---
+++ php day 2/hellophp.php
@@ -0,0 +1,14 @@
+<?php
+echo"welcome to php";
+echo"<h1>hello h1 tag in php</h1>";
+?>
+<html>
+    <br>
+    <body>
+        Hello Html<br>
+        <?php      
+         echo "php inside html";
+        ?>
+    </body>
+    
+</html>
